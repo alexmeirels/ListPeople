ea8d3c49f0fbe969736f35caaaf3268a5cf52698
Alex Meireles <alex.meireles@softruck.com>
2022-08-10 13:57:38 -0300

    installed Axios and used the API to create a list of people

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Header from './src/components/Header';
+import axios from 'axios';
 
-const RenderList = () => {
-  const names = ['Joao', 'Maria', 'Pedro', 'Matheus'];
+const App = props => {
+  const [peoples, setPeoples] = useState([]);
 
-  const textElements = names.map(name => {
-    return <Text>{name}</Text>;
-  });
-
-  return textElements;
-};
-
-const App = () => {
+  const ComponentDidMount = () => {
+    axios.get('https://randomuser.me/api/?nat=br&results=5').then(response => {
+      const {results} = response.data;
+      setPeoples(results);
+    });
+  };
+  //ComponentDidMount();
+  const renderList = () => {
+    const textElements = peoples.map(people => {
+      const {first} = people.name;
+      return <Text key={first}>{first}</Text>;
+    });
+    return textElements;
+  };
   return (
     <View>
       <Text />
-      <Header title="People" />
+      <Header title="Peoples" />
+      {renderList()}
     </View>
   );
 };
