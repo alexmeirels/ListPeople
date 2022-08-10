c7406e6d27ed68aba5ce08574a07ac2d004d776b
Alex Meireles <alex.meireles@softruck.com>
2022-08-10 17:12:44 -0300

    Added each people's Avatar

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
 const PeopleListItem = props => {
   const {people} = props;
   const {first, last} = people.name;
+  const {thumbnail} = people.picture;
   return (
     <View style={style.line}>
+      <Image style={style.avatar} source={{uri: thumbnail}} />
       <Text style={style.lineText}>{`${first} ${last}`}</Text>
     </View>
   );
 };
 const style = StyleSheet.create({
   line: {
-    height: 40,
+    height: 70,
     borderBottomWidth: 1,
     borderBottomColor: '#bbbbbb',
     alignItems: 'center',
@@ -21,6 +23,14 @@ const style = StyleSheet.create({
   lineText: {
     fontSize: 30,
     paddingLeft: 30,
+    flex: 7,
+  },
+  avatar: {
+    aspectRatio: 1,
+    width: 40,
+    flex: 1,
+    marginLeft: 30,
+    borderRadius: 50,
   },
 });
 
