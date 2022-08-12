608d1b43926099784e5cb4df3e9e06dbf3474aaa
Alex Meireles <alex.meireles@softruck.com>
2022-08-12 09:22:57 -0300

    Version bug fixed

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import {createAppContainer, createStackNavigator} from 'react-navigation';
-
+import {createStackNavigator} from '@react-navigation/stack';
 import PeoplePage from './src/screens/PeoplePage';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
-const AppNavigator = createStackNavigator({
-  Main: {
-    screen: PeoplePage,
-  },
-});
+const Stack = createStackNavigator();
 
-const AppContainer = createAppContainer(AppNavigator);
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="PeoplePage" component={PeoplePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default AppContainer;
+export default MyStack;
