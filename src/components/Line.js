0680db6e3bd84556f9efade86c9f03711f9fda36
Alex Meireles <alex.meireles@softruck.com>
2022-08-16 18:03:26 -0300

    added people information

---
+++ src/components/Line.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+const Line = ({label, content}) => {
+  return (
+    <View style={style.line}>
+      <Text style={[style.cell, style.Label]}>{label}</Text>
+      <Text style={style.cell}>{content}</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  line: {
+    flexDirection: 'row',
+    paddingTop: 3,
+    paddingBottom: 3,
+    borderWidth: 1,
+    borderColor: '#C5C5C5',
+  },
+  cell: {
+    fontSize: 18,
+    paddingLeft: 5,
+  },
+  Label: {
+    fontWeight: 'bold',
+  },
+});
+
+export default Line();
--- src/screens/PeopleDetailPage.js
+++ src/screens/PeopleDetailPage.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import {View, Text, Image, StyleSheet} from 'react-native';
+import Line from '../components/Line';
 export default class PeopleDetailPage extends React.Component {
   render() {
+    const {people} = this.props.route.params;
     return (
-      <View>
-        <Text>teste</Text>
+      <View style={style.container}>
+        <Image source={{uri: people.picture.large}} style={style.avatar} />
+        <View style={style.detailContainer}>
+          <View style={style.line}>
+            <Text style={[style.cell, style.label]}>Email: </Text>
+            <Text style={style.cell}>{people.email}</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const style = StyleSheet.create({
+  avatar: {
+    aspectRatio: 1,
+    borderRadius: 200,
+  },
+  container: {
+    padding: 15,
+  },
+  detailContainer: {
+    backgroundColor: '#e2f9ff',
+    marginTop: 20,
+    elevation: 1,
+  },
+});
